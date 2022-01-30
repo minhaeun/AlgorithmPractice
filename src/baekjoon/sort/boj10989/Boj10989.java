27bad176dfc8cae11792e0461b717ad74fe96c74
minhaeun <haeunee6575@gmail.com>
2022-01-30 23:41:18 +0900

    20220130

--- src/baekjoon/sort/boj10989/Boj10989.java
+++ src/baekjoon/sort/boj10989/Boj10989.java
@@ -1,4 +1,29 @@
 package baekjoon.sort.boj10989;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Boj10989 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for(int i = 0; i < n; i++){
+            sb.append(arr[i]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
 }
