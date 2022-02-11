e56add1dc7d495429c1ec8ccf8e5f3a4971768d9
minhaeun <haeunee6575@gmail.com>
2022-02-11 17:17:08 +0900

    20220211

---
+++ src/baekjoon/boj2309/Boj2309.java
@@ -0,0 +1,43 @@
+package baekjoon.boj2309;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Boj2309 {
+    static int N;
+    static int[] height;
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("./src/baekjoon/boj2309/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = 9;
+        height = new int[N];
+        int sum = 0;
+
+        for(int i = 0; i < N; i++){
+            height[i] = Integer.parseInt(br.readLine());
+            sum += height[i];
+        }
+
+        Arrays.sort(height);
+        int x1 = 0, x2 = 0;
+        for(int i = 0; i < 8; i++){
+            for(int j = i+1; j < 9; j++){
+                if(sum - height[i] - height[j] == 100){
+                   x1 = i;
+                   x2 = j;
+                }
+            }
+        }
+
+        for(int i = 0; i < 9; i++){
+            if(i != x1 && i != x2)
+                System.out.println(height[i]);
+        }
+
+
+
+    }
+}
