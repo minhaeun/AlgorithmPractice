e488782f16e7f1a0b13fc10174d4c8645865931d
minhaeun <haeunee6575@gmail.com>
2022-02-14 17:37:20 +0900

    20220214

---
+++ src/baekjoon/boj3040/Boj3040.java
@@ -0,0 +1,42 @@
+package baekjoon.boj3040;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Boj3040 {
+    static int[] input, numbers;
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("./src/baekjoon/boj3040/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        input = new int[9];
+        numbers = new int[7];
+        for(int i = 0; i < 9; i++){
+            input[i] = Integer.parseInt(br.readLine());
+        }
+        combination(0, 0);
+    }
+
+    public static void combination(int count, int start){
+
+        if(count == 7){
+            int total = 0;
+            for(int i = 0; i < 7; i++){
+                total += numbers[i];
+            }
+            if(total == 100){
+                for(int i = 0; i < 7; i++){
+                    System.out.println(numbers[i]);
+                }
+            }
+            return;
+        }
+        for(int i = start; i < 9; i++){
+            numbers[count] = input[i];
+            combination(count + 1, i + 1);
+        }
+    }
+}
