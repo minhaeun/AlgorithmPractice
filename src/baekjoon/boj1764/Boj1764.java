ae40a0c8e49a87d0650e4992ae3a23bb0b261c21
minhaeun <haeunee6575@gmail.com>
2022-02-28 23:18:42 +0900

    20220228

---
+++ src/baekjoon/boj1764/Boj1764.java
@@ -0,0 +1,36 @@
+package baekjoon.boj1764;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+
+public class Boj1764{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        int n = Integer.parseInt(inputs[0]);
+        int m = Integer.parseInt(inputs[1]);
+
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            String tmp = br.readLine();
+            if(set.contains(tmp)){
+                result.add(tmp);
+            }
+        }
+
+        Collections.sort(result);
+
+        System.out.println(result.size());
+        for (String s : result) {
+            System.out.println(s);
+        }
+    }
+}
