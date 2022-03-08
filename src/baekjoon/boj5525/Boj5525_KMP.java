738ec3988404825bd3945c1ade2f5aa9e4853abc
minhaeun <haeunee6575@gmail.com>
2022-03-08 21:39:51 +0900

    20220308

---
+++ src/baekjoon/boj5525/Boj5525_KMP.java
@@ -0,0 +1,63 @@
+package baekjoon.boj5525;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Boj5525_KMP {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("./src/baekjoon/boj5525/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int S = Integer.parseInt(br.readLine());
+        char[] text = br.readLine().toCharArray();
+        char[] pattern = new char[2*N+1];
+
+        for(int i = 0; i < 2*N+1; i+=2){
+            pattern[i] = 'I';
+        }
+        for(int i = 1; i < 2*N+1; i+=2){
+            pattern[i] = 'O';
+        }
+
+        int tLength = text.length, pLength = pattern.length;
+        int[] pi = new int[pLength]; // 부분일치 테이블
+
+        for(int i = 1, j = 0; i < pLength; i++){
+            while(j > 0 && pattern[i] != pattern[j]){
+                j = pi[j - 1];
+            }
+            if(pattern[i] == pattern[j]){
+                pi[i] = ++j;
+            }
+            else    pi[i] = 0;
+        }
+
+        int count = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0, j = 0; i < tLength; i++){
+            while(j > 0 && text[i] != pattern[j]){
+                j = pi[j - 1];
+            }
+
+            if(text[i] == pattern[j]){
+                if(j == pLength - 1){
+                    count++;
+                    list.add((i + 1) - pLength);
+                    j = pi[j];
+                }
+                else{
+                    j++;
+                }
+            }
+        }
+        System.out.println(count);
+
+
+
+
+    }
+}
---
+++ src/baekjoon/boj9019/Boj9019.java
@@ -0,0 +1,92 @@
+package baekjoon.boj9019;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Boj9019 {
+    private static class Calculator {
+        int register;
+        String history;
+
+        Calculator(int register, String history){
+            this.register = register;
+            this.history = history;
+        }
+    }
+
+    static int T, A, B;
+    static Queue<Calculator> q = new LinkedList<>();
+    static boolean[] visited;
+
+    public static void main(String[] args) throws Exception{
+        System.setIn(new FileInputStream("./src/baekjoon/boj9019/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        T = Integer.parseInt(br.readLine());
+
+        for(int i = 0 ; i < T ; ++i) {
+            st = new StringTokenizer(br.readLine(), " ");
+            A = Integer.parseInt(st.nextToken());
+            B = Integer.parseInt(st.nextToken());
+
+            visited = new boolean[10000];
+            visited[A] = true;
+            q.offer(new Calculator(A, ""));
+            bfs();
+            q.clear();
+        }
+    }
+
+    private static void bfs() {
+        while(!q.isEmpty()) {
+            Calculator cal = q.poll();
+
+            if(cal.register == B) {
+                System.out.println(cal.history);
+                return;
+            }
+
+            int d = D(cal.register);
+            int s = S(cal.register);
+            int l = L(cal.register);
+            int r = R(cal.register);
+
+            if(!visited[d]) {
+                visited[d] = true;
+                q.offer(new Calculator(d, cal.history + "D"));
+            }
+            if(!visited[s]) {
+                visited[s] = true;
+                q.offer(new Calculator(s, cal.history + "S"));
+            }
+            if(!visited[l]) {
+                visited[l] = true;
+                q.offer(new Calculator(l, cal.history + "L"));
+            }
+            if(!visited[r]) {
+                visited[r] = true;
+                q.offer(new Calculator(r, cal.history + "R"));
+            }
+        }
+    }
+
+    private static int D(int number) {
+        return (number * 2) % 10000;
+    }
+
+    private static int S(int number) {
+        return number == 0 ? 9999 : number - 1;
+    }
+
+    private static int L(int number) {
+        return ((number % 1000) * 10) + (number / 1000);
+    }
+
+    private static int R(int number) {
+        return ((number % 10) * 1000) + (number / 10);
+    }
+}
