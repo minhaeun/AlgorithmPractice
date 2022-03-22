4986e3b6b0f48bcbf6845a53193944952e6eb309
minhaeun <haeunee6575@gmail.com>
2022-03-22 22:37:01 +0900

    20220322

---
+++ src/baekjoon/boj1008/Boj1008.java
@@ -0,0 +1,18 @@
+package baekjoon.boj1008;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj1008 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("./src/baekjoon/boj1008/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        double A = Double.parseDouble(st.nextToken());
+        double B = Double.parseDouble(st.nextToken());
+
+        System.out.println(A / B);
+    }
+}
