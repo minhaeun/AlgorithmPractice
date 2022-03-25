fa7542533dc6b843d293ddf09e592d510feefa7a
minhaeun <haeunee6575@gmail.com>
2022-03-25 23:21:40 +0900

    20220325

---
+++ src/baekjoon/boj15657/Boj15657.java
@@ -0,0 +1,48 @@
+package baekjoon.boj15657;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Boj15657 {
+    static int N, M;
+    static int[] arr, result;
+    static boolean[] visit;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("./src/baekjoon/boj15657/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        result = new int[M];
+        visit = new boolean[N];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        dfs(0, 0);
+        System.out.println(sb.toString());
+    }
+
+    static void dfs(int start, int dept){
+        if(dept == M){
+            for(int i = 0; i < M; i++){
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i = start; i < N; i++){
+            result[dept] = arr[i];
+            dfs(i, dept + 1);
+        }
+    }
+}
