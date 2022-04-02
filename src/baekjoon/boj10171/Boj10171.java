e8ec1b24d0adc8510c8b82025eac1eef4afb5370
minhaeun <haeunee6575@gmail.com>
2022-04-02 14:10:46 +0900

    20220402

---
+++ src/baekjoon/boj10171/Boj10171.java
@@ -0,0 +1,10 @@
+package baekjoon.boj10171;
+
+public class Boj10171 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\\n").append(" )  ( ')\n").append("(  /  )\n").append(" \\(__)|\n");
+
+        System.out.println(sb.toString());
+    }
+}
