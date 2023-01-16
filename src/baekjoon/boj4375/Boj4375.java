60c00f0b344536d88b21fcde363784816c069014
minhaeun <haeunee6575@gmail.com>
2023-01-17 00:28:30 +0900

    20230117

---
+++ src/baekjoon/boj4375/Boj4375.java
@@ -0,0 +1,24 @@
+package baekjoon.boj4375;
+
+import java.io.*;
+
+public class Boj4375 {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("./src/baekjoon/boj4375/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String input;
+        while ((input = br.readLine()) != null) {
+            int exp = 1;
+            long base = 1;
+            int num = Integer.parseInt(input);
+
+            while ((base = base % num) != 0) {
+                base = base*10 + 1;
+                exp++;
+            }
+            sb.append(exp).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
