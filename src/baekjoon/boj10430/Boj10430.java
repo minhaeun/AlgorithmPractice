d1abbd36b9434d3bd041cedd9f7f296b9b84aa7a
minhaeun <haeunee6575@gmail.com>
2023-01-17 19:55:51 +0900

    20230117

--- src/baekjoon/boj10430/Boj10430.java
+++ src/baekjoon/boj10430/Boj10430.java
@@ -1,12 +1,10 @@
 package baekjoon.boj10430;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Boj10430 {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("./src/baekjoon/boj10430/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -16,8 +14,7 @@ public class Boj10430 {
         int B = Integer.parseInt(st.nextToken());
         int C = Integer.parseInt(st.nextToken());
 
-        sb.append((A+B)%C).append("\n").append(((A%C) + (B%C))%C).append("\n").append((A*B)%C).append("\n").append(((A%C)*(B%C))%C);
-        System.out.println(sb.toString());
-
+        sb.append((A+B)%C).append("\n").append(((A%C) + (B%C))%C).append("\n").append((A*B)%C).append("\n").append(((A%C) * (B%C))%C);
+        System.out.println(sb);
     }
 }
