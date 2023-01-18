2f01ff932568114843f03a1e21e3e68b9b35395a
minhaeun <haeunee6575@gmail.com>
2023-01-18 21:44:55 +0900

    20230118

--- src/baekjoon/mathematics/boj1037/Boj1037_2.java
+++ src/baekjoon/mathematics/boj1037/Boj1037_2.java
@@ -1,14 +1,13 @@
-package baekjoon.boj1037;
+package baekjoon.mathematics.boj1037;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Boj1037_2 {
     public static void main(String[] args) throws Exception {
-        System.setIn(new FileInputStream("./src/baekjoon/boj1037/input.txt"));
+        System.setIn(new FileInputStream("baekjoon/mathematics/boj1037/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int arr[] = new int[N];
