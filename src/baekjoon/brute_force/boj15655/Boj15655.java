75ea40dbea1bef45cef7d42629ea8ff1df8cd4bf
minhaeun <haeunee6575@gmail.com>
2023-02-01 22:54:28 +0900

    20230201

---
+++ src/baekjoon/brute_force/boj15655/Boj15655.java
@@ -0,0 +1,55 @@
+package baekjoon.brute_force.boj15655;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Boj15655 {
+    static int N, M, nums[], ans[];
+    static boolean visited[];
+    static StringBuilder sb;
+    public static void main(String[] args) throws Exception{
+        System.setIn(new FileInputStream("./src/baekjoon/brute_force/boj15655/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        nums = new int[N];
+        visited = new boolean[N];
+        ans = new int[M];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        dfs(0, 0);
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int count, int start){
+        if(count == M){
+            for(int i = 0; i < M; i++){
+                sb.append(ans[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = start; i < N; i++){
+            if(visited[i])  continue;
+            visited[i] = true;
+            ans[count] = nums[i];
+            dfs(count + 1, i);
+            visited[i] = false;
+        }
+
+    }
+}
