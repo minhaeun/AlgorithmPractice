69bdb0710ad9819fd702f043c9e458f9bf92f833
minhaeun <haeunee6575@gmail.com>
2023-02-06 20:03:12 +0900

    20230206

---
+++ src/baekjoon/data_structure/boj11660/Boj11660.java
@@ -0,0 +1,48 @@
+package baekjoon.data_structure.boj11660;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj11660 {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("./src/baekjoon/data_structure/boj11660/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] A = new int[N + 1][N + 1];
+        int[][] D = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 1; j <= N; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                D[i][j] = D[i - 1][j] + D[i][j - 1] - D[i - 1][j - 1] + A[i][j];
+            }
+        }
+
+        for (int m = 1; m <= M; m++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int sum = D[x2][y2] - D[x2][y1 - 1] - D[x1 - 1][y2] + D[x1 - 1][y1 - 1];
+            sb.append(sum).append("\n");
+
+        }
+        System.out.println(sb);
+
+
+    }
+}
