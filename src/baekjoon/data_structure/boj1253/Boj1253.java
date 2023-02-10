9313275c1aebe1b8aab74e2a2d52fc1cdea0c7cb
minhaeun <haeunee6575@gmail.com>
2023-02-10 22:22:19 +0900

    20230210

---
+++ src/baekjoon/data_structure/boj1253/Boj1253.java
@@ -0,0 +1,51 @@
+package baekjoon.data_structure.boj1253;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Boj1253 {
+    public static void main(String[] args) throws Exception{
+        System.setIn(new FileInputStream("./src/baekjoon/data_structure/boj1253/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for(int i = 0; i < N; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+        int count = 0;
+
+        for(int i = 0; i < N; i++){
+            int start_index = 0;
+            int end_index = N - 1;
+
+            while(start_index < end_index){
+                int sum = A[start_index] + A[end_index];
+                if(sum < A[i]){
+                    start_index++;
+                }else if(sum > A[i]){
+                    end_index--;
+                }else{
+                    if(start_index == i)    start_index++;
+                    else if(end_index == i) end_index--;
+                    else{
+                        count++;
+                        break;
+                    }
+
+                }
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
