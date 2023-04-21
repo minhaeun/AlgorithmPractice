f0b59a2f691e764675987f58e5a5f984baa83c9a
minhaeun <haeunee6575@gmail.com>
2023-04-21 20:35:48 +0900

    20230421

---
+++ src/baekjoon/boj1703/Boj1703.java
@@ -0,0 +1,30 @@
+package baekjoon.boj1703;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj1703 {
+    public static void main(String[] args) throws Exception{
+        System.setIn(new FileInputStream("./src/baekjoon/boj1703/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        while(true){
+            st = new StringTokenizer(br.readLine(), " ");
+            int N = Integer.parseInt(st.nextToken());
+            int tree = 1;
+
+            if(N == 0)  break;
+            for(int i = 0; i < 2 * N; i++){
+                int M = Integer.parseInt(st.nextToken());
+                if(i % 2 == 0)  tree *= M;
+                else tree -= M;
+            }
+            System.out.println(tree);
+        }
+
+
+    }
+}
