9ff6f65a348c1bf2834ca3e47d10ba14bc433a7d
minhaeun <haeunee6575@gmail.com>
2025-04-22 07:04:41 +0900

    20250421

--- src/programmers/kakao2022/P92335.java
+++ src/programmers/kakao2022/P92335.java
@@ -6,15 +6,17 @@ public class P92335 {
     public static int solution(int n, int k) {
         int answer = 0;
 
-        String str = Integer.toString(n, k);
+        String str = Long.toString(n, k);
         StringTokenizer st = new StringTokenizer(str, "0");
         while (st.hasMoreTokens()) {
-            int num = Integer.parseInt(st.nextToken(), k);
+            long num = Long.parseLong(st.nextToken(), 10);
             if(isPrime(num)) answer++;
         }
         return answer;
     }
-    public static boolean isPrime(int num) {
+    public static boolean isPrime(long num) {
+        if(num < 2) return false;
+        if(num == 2) return true;
         for(int i = 2; i <= Math.sqrt(num); i++) {
             if(num % i == 0) return false;
         }
