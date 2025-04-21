e67315b9c9ccb559f4d23892200114994cf64084
minhaeun <haeunee6575@gmail.com>
2025-04-22 06:49:30 +0900

    20250421

---
+++ src/programmers/kakao2022/P92335.java
@@ -0,0 +1,30 @@
+package programmers.kakao2022;
+
+import java.util.StringTokenizer;
+
+public class P92335 {
+    public static int solution(int n, int k) {
+        int answer = 0;
+
+        String str = Integer.toString(n, k);
+        StringTokenizer st = new StringTokenizer(str, "0");
+        while (st.hasMoreTokens()) {
+            int num = Integer.parseInt(st.nextToken(), k);
+            if(isPrime(num)) answer++;
+        }
+        return answer;
+    }
+    public static boolean isPrime(int num) {
+        for(int i = 2; i <= Math.sqrt(num); i++) {
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int n1 = 437674, k1 = 3;
+        int n2 = 110011, k2 = 10;
+
+        System.out.println(solution(n1, k1));
+        System.out.println(solution(n2, k2));
+    }
+}
