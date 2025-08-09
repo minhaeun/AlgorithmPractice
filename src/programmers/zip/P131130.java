4b400cb122ef6c08c67f0cbfa06b9c79415372a8
minhaeun <haeunee6575@gmail.com>
2025-08-09 18:24:18 +0900

    20250809

---
+++ src/programmers/zip/P131130.java
@@ -0,0 +1,49 @@
+package programmers.zip;
+
+import java.util.Arrays;
+
+public class P131130 {
+    public static int count, visited[], len;
+    public static int solution(int[] cards) {
+        int answer = 1;
+        count = 1;
+        len = cards.length;
+        visited = new int[len];
+
+
+        for(int i = 0; i < len; i++){
+            if(visited[i] != 0) continue;
+            visited[i] = count;
+            dfs(cards[i] - 1, cards);
+        }
+        int[] arr = new int[count];
+
+        for(int i = 0; i < len; i++){
+            int value = visited[i];
+            arr[value - 1]++;
+        }
+
+        Arrays.sort(arr);
+        answer = arr[count - 1] * arr[count - 2];
+        return answer;
+    }
+
+    public static void dfs(int num, int[] cards){
+
+        if(visited[num] != 0){
+            count++;
+            return;
+        }
+
+        visited[num] = count;
+        int value = cards[num];
+        dfs(value - 1, cards);
+    }
+
+
+    public static void main(String[] args) {
+        int[] cards = {8,6,3,7,2,5,1,4};
+        System.out.println(solution(cards));
+
+    }
+}
